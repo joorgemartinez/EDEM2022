04df574008e3abaef9e30f1e931662ac0884c9ee
Jorge <martinezca.jorge@gmail.com>
2022-11-25 21:09:06 +0100

    Repaso Python

---
+++ PYTHON/REPASO/Models/armario.py
@@ -0,0 +1,9 @@
+from producto import Producto
+
+class Armario():
+    puertas: int
+    altura: float
+    ancho: float
+
+    def __init__():
+    
--- PYTHON/REPASO/Models/producto.py
+++ PYTHON/REPASO/Models/producto.py
@@ -3,9 +3,27 @@ from enum import Enum
 class Categoria(Enum):
     COCINA = 1
     SALON = 2
-    
+    TERRAZA = 3
+
+class Color(Enum):
+    BLANCO = 1
+    NEGRO = 2
+    MARRON = 3
 
 class Producto():
     nombre: str
     precio: float
-    categoria: str
+    categoria: Categoria
+    coste: float
+    color: Color
+
+    def __init__(self, nombre:str, precio:float, categoria: Categoria, coste:float, color:Color):
+        self.nombre = nombre
+        self.precio = precio
+        self.categoria = categoria 
+        self.coste = coste
+        self.color = color
+
+
+        
+     
